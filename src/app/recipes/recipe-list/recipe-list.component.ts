0b48b3e8e4ee464ccef63ba63ef31bfdc0a67e92
unknown <batuhan.balaban@yahoo.com>
2019-10-05 14:51:14 +0300

    binding data to child comp. (PropertyBinding and @input Decorator)

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,7 +8,9 @@ import {Recipe} from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes : Recipe[] = [
-    new Recipe('Test Recipe','Test Description','https://media.defense.gov/2015/Nov/16/2001318253/-1/-1/0/151123-A-YG824-001.JPG')
+    new Recipe('Test Recipe','Test Description','https://media.defense.gov/2015/Nov/16/2001318253/-1/-1/0/151123-A-YG824-001.JPG'),
+    new Recipe('Other Recipe','Other Description','https://food.fnr.sndimg.com/content/dam/images/food/fullset/2012/11/2/0/DV1510H_fried-chicken-recipe-10_s4x3.jpg.rend.hgtvcom.826.620.suffix/1568222255998.jpeg')
+
   ];
 
   constructor() { }
